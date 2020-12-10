951c1e457e87c51e3240d6525edc54fc0eb8e908
johan <fiskgrodan@hotmail.com>
2020-12-11 00:23:29 +0100

    fix: remove comments

--- rollup.config.js
+++ rollup.config.js
@@ -13,17 +13,8 @@ export default {
 		file: 'public/game.js'
 	},
 	plugins: [
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
-		resolve({	browser: true }),
-
+		resolve({ browser: true }),
 		commonjs(),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
 		production && terser()
 	]
 };
